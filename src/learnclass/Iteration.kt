8bb142a2830d79765aede179b15ffaa1ab8efe8a
Griotold <gotjd9773@naver.com>
2024-02-18 17:34:53 +0900

    [기본 문법] - in으로 범위 검사

--- src/learnclass/Iteration.kt
+++ src/learnclass/Iteration.kt
@@ -22,4 +22,15 @@ fun main() {
         2 to "Diana",
         3 to "Frost"
     )
+
+    println("a는 ${recognize('a')}")
+    println("3는 ${recognize('3')}")
+    println("@는 ${recognize('@')}")
+}
+
+// in으로 범위검사
+fun recognize(c:Char) = when(c) {
+    in '0'..'9' -> "숫자"
+    in 'a'..'z', in 'A'..'Z' -> "알파벳"
+    else -> "숫자도 문자도 아님"
 }

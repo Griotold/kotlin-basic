428748352d71dddbab5cab8da0b04f88a3b09bec
Griotold <gotjd9773@naver.com>
2024-04-13 15:05:37 +0900

    [sparta] 실습 - 텍스트 RPG 게임 틀 만들기

---
+++ src/sparta/Text.kt
@@ -0,0 +1,16 @@
+package sparta
+
+fun main() {
+    var worldName = "스코월드"
+    var myName = "Griotold"
+    var myAge = 15
+    var myJob = "마법사"
+
+    println("=====================${worldName}에 오신 것을 환영합니다.=================")
+    println("당신의 정보는 다음과 같습니다.")
+    println("이름: ${myName}")
+    println("나이: ${myAge}")
+    println("직업: ${myJob}")
+    println("나이: ${myAge}")
+    println("모험을 떠나 볼까요?")
+}
